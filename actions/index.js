41a1b6e4e02a7716a32383e023cc1929e3d68c4b
Michael Anderson <michanders@msn.com>
2018-03-01 22:20:04 +0000

    automatically backed up by learn

--- actions/index.js
+++ actions/index.js
@@ -1,24 +1,69 @@
 'use strict';
 
 function addColumn (ev) {
+  ev.preventDefault();
+  
+  this.setState({
+    table: this.state.table.map(row => [...row, ''])
+  })
 }
 
 function addRow (ev) {
+  ev.preventDefault();
+
+  const width = this.state.table[0].length;
+
+  this.setState({
+    table: [...this.state.table, new Array(width).fill('')]
+  })
 }
 
 function changeCell (rowIndex, columnIndex, ev) {
+  const updatedTable = this.state.table;
+  updatedTable[rowIndex][columnIndex] = ev.target.value;
+  
+  this.setState({
+    table: updatedTable
+  })
 }
 
 function focusCell (rowIndex, columnIndex) {
+  this.setState({
+    focused: [rowIndex, columnIndex]
+  })
 }
 
 function blurCell () {
+  this.setState({
+    focused: null
+  })
 }
 
 function removeRow (ev) {
+  ev.preventDefault();
+
+  const updatedTable = this.state.table;
+  if (updatedTable.length > 1) {
+    updatedTable.pop();
+    this.setState({
+      table: updatedTable
+    })
+  }
 }
 
 function removeColumn (ev) {
+  ev.preventDefault();
+  const width = this.state.table[0].length;
+
+  if (width > 1) {
+    const updatedTable = this.state.table.map((row) => {
+      row.pop();
+      return row;
+    })
+    this.setState({
+      table: updatedTable
+    })
+  }
 }
 
 export default {
