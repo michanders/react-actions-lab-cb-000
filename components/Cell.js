4eaac7d395a3c777da553db7e3f5185e58cc6d72
Michael Anderson <michanders@msn.com>
2018-03-01 22:02:11 +0000

    automatically backed up by learn

--- components/Cell.js
+++ components/Cell.js
@@ -5,7 +5,7 @@ import React from 'react';
 export default class Cell extends React.Component {
   render () {
     return (
-      <td>
+      <td class="cell">
         <Cell  />
       </td>
     )
