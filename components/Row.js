3e93d1de82522aa3c7238f57ee9eb834622ede75
Michael Anderson <michanders@msn.com>
2018-03-01 22:13:03 +0000

    automatically backed up by learn

--- components/Row.js
+++ components/Row.js
@@ -7,18 +7,15 @@ export default class Row extends React.Component {
   render () {
     return (
       <tr className="row">
-        {
-          this.props.row.map((cell, index) =>
-            <Cell 
-              key={index}
-              cell={cell}
-              onChange={this.props.onChange.bind(null,index)}
-              onFocus={this.props.onFocus.bind(null,index)} />
-          )
-        }
+        {this.props.row.map((cell, index) =>
+          <Cell
+            key={index}
+            cell={cell}
+            onChange={this.props.onChange.bind(null,index)}
+            onFocus={this.props.onFocus.bind(null,index)}
+            />
+          )}
       </tr>
     )
-  }
-}
   }
 }
