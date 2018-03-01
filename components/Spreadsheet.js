1d5e771727b0951a335c79f256b5fdac41993ecf
Michael Anderson <michanders@msn.com>
2018-03-01 22:23:29 +0000

    automatically backed up by learn

--- components/Spreadsheet.js
+++ components/Spreadsheet.js
@@ -13,27 +13,38 @@ export default class Spreadsheet extends React.Component {
         ['']
       ]
     };
+
+    this.handleAddColumn    =   actions.addColumn.bind(this);
+    this.handleAddRow       =   actions.addRow.bind(this);
+    this.handleRemoveColumn =   actions.removeColumn.bind(this);
+    this.handleRemoveRow    =   actions.removeRow.bind(this);
+    this.handleChange       =   actions.changeCell.bind(this);
+    this.handleFocus        =   actions.focusCell.bind(this);
+    this.handleBlur         =   actions.blurCell.bind(this);
+
   }
+
 
   render () {
     return (
       <div className='spreadsheet'>
         <h1>My little spreadsheet</h1>
+
         <div className='spreadsheet__buttons'>
-          <button onClick={actions.addColumn.bind(this)}>Add Column</button>
+          <button onClick={this.handleAddColumn}>Add Column</button>
           {' '}
-          <button onClick={actions.addRow.bind(this)}>Add Row</button>
+          <button onClick={this.handleAddRow}>Add Row</button>
           {' '}
-          <button onClick={actions.removeColumn.bind(this)}>Remove Column</button>
+          <button onClick={this.handleRemoveColumn}>Remove Column</button>
           {' '}
-          <button onClick={actions.removeRow.bind(this)}>Remove Row</button>
+          <button onClick={this.handleRemoveRow}>Remove Row</button>
         </div>
 
         <Table
           table={this.state.table}
-          onChange={actions.changeCell.bind(this)}
-          onFocus={actions.focusCell.bind(this)}
-          onBlur={actions.blurCell.bind(this)} />
+          onChange={this.handleChange}
+          onFocus={this.handleFocus}
+          onBlur={this.handleBlur} />
 
         <div className='spreadsheet__focused'>
           {this.state.focused && this.state.focused.join(' - ')}
